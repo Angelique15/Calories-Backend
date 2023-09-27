e4c7e4b8e94cd931c85b0c141fcc709b828c76e2
Angelique15 <mar.angel.go15@gmail.com>
2023-09-27 14:22:03 -0500

    netlify-config

--- server.js
+++ server.js
@@ -1,70 +1,55 @@
 const express = require('express');
 const cors = require('cors');
-const connection = require("./db/connection");
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
 };
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-//ruta para obtener alimentos no permitidos por tipo de sangre
+// Rutas para obtener alimentos no permitidos por tipo de sangre
 const notAllowedFoodsRoutes = require('./routes/notAllowedFoodsRoutes');
-
 app.use('/api/not-allowed-foods', notAllowedFoodsRoutes);
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true, // Permite el uso de credenciales en las solicitudes
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
+require('./config/config-passport');
 
-require("./config/config-passport");
-
-const routerApi = require("./api");
-app.use("/api", routerApi);
+const routerApi = require('./api');
+app.use('/api', routerApi);
 
 app.use((_, res) => {
   res.status(404).json({
-    status: "error",
+    status: 'error',
     code: 404,
     message: `Use api on routes: 
-    /api/signup - registration user {username, email, password}
-    /api/login - login {email, password}`,
-    data: "Not found",
+  /api/signup - registration user {username, email, password}
+  /api/login - login {email, password}`,
+    data: 'Not found',
   });
 });
 
 app.use((err, _, res) => {
   console.log(err.stack);
   res.status(500).json({
-    status: "fail",
+    status: 'fail',
     code: 500,
     message: err.message,
-    data: "Internal Server Error",
+    data: 'Internal Server Error',
   });
 });
 
-const PORT = process.env.PORT || 3001;
-
-connection
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Database connection successful on port: ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(`Server not running. Error .message ${err.message}`);
-    process.exit(1);
-  });
-
-
-// // Ruta para obtener los alimentos no recomendados según el tipo de sangre
+// Ruta para obtener los alimentos no recomendados según el tipo de sangre
 app.get('/api/not-allowed-foods/:bloodType', (req, res) => {
   const { bloodType } = req.params;
   // Lee el archivo JSON con los alimentos no recomendados desde la carpeta models
@@ -75,4 +60,8 @@ app.get('/api/not-allowed-foods/:bloodType', (req, res) => {
 
   res.json(foodsForBloodType);
 });
+
+// Exporta la aplicación Express como una función
+module.exports = app;
+
 
